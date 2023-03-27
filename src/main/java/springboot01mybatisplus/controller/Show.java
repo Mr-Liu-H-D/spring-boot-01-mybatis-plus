796f45be5dfccca31cd15f00f0f29e45735f0d2b
dong <3316179099@qq.com>
2023-03-27 15:30:12 +0800

    第三次修改

---
+++ src/main/java/springboot01mybatisplus/controller/Show.java
@@ -0,0 +1,17 @@
+package springboot01mybatisplus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author 你的名字
+ * @Date: 2023/3/27 15:10
+ * @Instructions:
+ */
+@Controller
+public class Show {
+  @RequestMapping("/grues")
+    public String getGrues(){
+    return null;
+  }
+}
--- src/main/java/springbootapplication/controller/Show.java
+++
@@ -1,9 +0,0 @@
-package springbootapplication.controller;
-
-/**
- * @Author 你的名字
- * @Date: 2023/3/27 14:35
- * @Instructions:
- */
-public class Show {
-}
